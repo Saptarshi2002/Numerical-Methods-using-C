cbf9edd2a924bd4efc04f5c2a7f2524b0d967932
unknown <saptarshim44@gmail.com>
2023-02-21 10:01:55 +0530

    First Commit

---
+++ Newton Raphson/Trapezoidal rule/trpezoidal.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+float f(float x)
+{
+    return x*x*x;
+}
+
+int main()
+{
+    float a,b,n,sum=0,i,h;
+    printf("Enter the value of a ");
+    scanf("%f",&a);
+
+    printf("Enter the value of b ");
+    scanf("%f",&b);
+
+    printf("Enter number of steps ");
+    scanf("%f",&n);
+
+    h=(b-a)/n;
+
+    sum=f(a)+f(b);
+
+    for(i=a+h;i<b;i=i+h)
+    {
+        sum=sum+(2*f(i));
+    }
+
+    sum=(sum*h)/2;
+
+    printf("\nThe value of the integral if %f",sum);
+
+}
